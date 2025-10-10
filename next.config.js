12948d8f72802a0b927c9cdef2a9ab329d4684d1
shivamVishwakarma212 <shivamviswakarma@askgalore.com>
2025-10-11 00:17:51 +0530

    Fix CSS and static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
     unoptimized: true
   },
   distDir: 'out',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/shramik-setu' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/shramik-setu' : '',
   experimental: {
     appDir: true,
   },
